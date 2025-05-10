7e4090577be23246de4cadc33ad0b6154484d8cc
Sharma7147 <118017416+Sharma7147@users.noreply.github.com>
2025-05-10 20:06:00 +0530

    Time: 0 ms (100%), Space: 42.1 MB (30%) - LeetHub

---
+++ 1449-print-words-vertically/1449-print-words-vertically.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Solution {
+    public List<String> printVertically(String s) {
+        String[] words = s.split(" ");
+        int maxLen = 0;
+
+        for (String word : words) {
+            maxLen = Math.max(maxLen, word.length());
+        }
+
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < maxLen; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (String word : words) {
+                if (i < word.length()) {
+                    sb.append(word.charAt(i));
+                } else {
+                    sb.append(' ');
+                }
+            }
+           
+            int end = sb.length();
+            while (end > 0 && sb.charAt(end - 1) == ' ') {
+                end--;
+            }
+            ans.add(sb.substring(0, end));
+        }
+
+        return ans;
+    }
+}
