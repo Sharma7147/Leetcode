851495e931ee5081f983a505a8b47d16a8b525b7
Sharma7147 <118017416+Sharma7147@users.noreply.github.com>
2025-06-18 19:24:46 +0530

    Time: 90 ms (7.17%), Space: 46.2 MB (60.79%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -13,16 +13,16 @@ class Solution {
         
         
 
-        for(int i=k+1;i<arr.length;i++){
-            if(arr[i]>target){
-                return arr[i];
+        for(int i=k+1;i<arr.length*2;i++){
+            if(arr[i%arr.length]>target){
+                return arr[i%arr.length];
             }
         }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>target){
-                return arr[i];
-            }
-        }
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>target){
+        //         return arr[i];
+        //     }
+        // }
 
         return -1;
 
