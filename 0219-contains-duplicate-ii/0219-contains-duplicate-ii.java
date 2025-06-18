8def9ff652fa0608968ed643eae7a4c8d2b252d9
Sharma7147 <118017416+Sharma7147@users.noreply.github.com>
2025-06-18 18:43:15 +0530

    Time: 1289 ms (5.06%), Space: 58.3 MB (93.13%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,14 +1,25 @@
 class Solution {
-    public boolean containsNearbyDuplicate(int[] a, int k) {
-        int max = 0;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length && j <= i + k; j++) {
-                if (a[i] == a[j]) {
-                    return true;
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        int i = 0;
+        int j = 1;
+        while (i < nums.length) {
+
+            while (j < nums.length) {
+                if (nums[i] == nums[j]) {
+                    if (j - i <= k)
+                        return true;
                 }
+                else{
+                    if(j-i>k)
+                    break;
+                }
+                j++;
+
             }
+            i++;
+            j = i + 1;
         }
         return false;
-
     }
 }
